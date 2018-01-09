18d337fcd07619877342d09d22f3638254fda64a
Sterling Simms <Sterling_simms@yahoo.com>
2018-01-09 12:03:31 -0600

    Finish assignment

---
+++ twoLists.py
@@ -0,0 +1,6 @@
+def twoLists(listOne,listTwo):
+    if listOne == listTwo:
+        print "The lists are the same!"
+    if listOne != listTwo: 
+        print "The lists are not the same!"
+twoLists([],[])
